9d7d726e5ac09e1993cc765a64f534bcfa83d6a9
Yasuel <146424368+Yasuel@users.noreply.github.com>
2024-10-11 15:19:22 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,24 +1,51 @@
 //funktio joka lisää tekstikentän syötteen listaan
-function submittargets() {
+function submittarget() {
     event.preventDefault();
     var x = document.getElementsByTagName('input');
     var syöte = x[0].value;
+    //tarkistetaan onko kentässä syötettä ja heitetään virheilmoitus konsolille
+    if (syöte.length < 1) {
+        throw new Error("Target can not be empty")
+    }
+    //lisätään uusi rivi
     var t = document.getElementById('list');
     var l =t.tBodies[0].rows.length;
     var r = t.insertRow(l);
-    var cell1 = r.insertCell(0);
-    cell1.innerHTML = syöte;
-    
-    
+    //luodaan ja lisätään checkbox
+    var cell2 = r.insertCell(0);
+    var nap = document.createElement("input")
+    nap.type = "checkbox"
+    nap.addEventListener("change", yliviivaa())
+    cell2.appendChild(nap)
+    //lisätään itse syötä listaan ja tyhjätään syötekenttä
+    var cell3 = r.insertCell(1);
+    cell3.innerHTML = syöte;
+    x[0].value = '';
+    //tällä tehdään poistonappula, 
+    //mutta koska en saa siihen toiminnallisuutta, 
+    //jätän sen vain kommentoituna tänne
+    //var cell1 = r.insertCell(2);
+    //var del = document.createElement("input")
+    //del.type = "submit"
+    //del.value = "Delete row"
+    //cell1.appendChild(del)
 }
 
 //kuuntelija listalle lisäämiseen
 var x = document.getElementsByTagName('input');
-x[1].addEventListener("click", submittargets);
+x[1].addEventListener("click", submittarget);
 
-//funktio listan tyhjentämiseen
+//funktio listan tyhjentämiseen (olikos tämä nyt JQuery eli kielletty?)
+function clearlist() {
+    var tbody = document.querySelector("#list tbody");
+    tbody.innerHTML = "";
+}
 
+//funktio rivin yliviivaamisen (mielekiinto loppui)
+function yliviivaa() {
+
+}
 
 //kuuntelija listan tyhjentämiseen
-
-//myös tehtävä funkito ja kuuntelija joilla merkitään asia listalla tehdyksi
+var z = document.getElementsByTagName('input')
+z[2].addEventListener("click", clearlist);
